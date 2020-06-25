2dc2c6f77f4c0f9fa35e79e79825796b556cf5cd
Brian <bmcewen609@gmail.com>
2020-06-25 16:41:27 +0000

    added .gitignore and sorted clone of data rater than data

--- public/words.js
+++ public/words.js
@@ -1,34 +1,25 @@
-//tally number of instances per city
-function tally(data) {
-	var sorted = data.sort((a, b) => {
-		return a.city > b.city ? 1 : -1;
-	});
-	var newData = [];
-	for (var i = 0; i < sorted.length; i++) {
-		var last = data[data.length - 1];
-		if (last && last.city === sorted[i]) last.tally++;
-		else
-			data.push({
-				city: sorted[i],
-				tally: 1
-			});
-	}
-	console.log(newData);
-}
-
-//increase text size due to increased instanced
+function getTally(cleanData){
+    cleanData.sort((a, b) => {
+      return a.city > b.city ? 1 : -1;
+    });
+    console.log(cleanData);
+  }
 
 // get data from csv
 d3
 	.csv('./data.csv', function(d) {
 		return {
-			city: d.City,
-			state: d.State
+			city: (`${d.City}, ${d.State}`)
 		};
 	})
+
+  //clone data so I am not messing with source
+  
+
 	.then(function(data) {
-		tally(data);
-		d3.select('body').selectAll('h2').data(data).enter().append('h2').text((d) => d.city + ', ' + d.state);
+    const cleanData = [...data]
+		getTally(cleanData);
+		d3.select('body').selectAll('h2').data(data).enter().append('h2').text((d) => d.city);
 	});
 
 //create svg
