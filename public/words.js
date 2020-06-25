542f9f1e93186dae9cb9a04430231594fa87d197
Brian <bmcewen609@gmail.com>
2020-06-25 22:33:56 +0000

    got tally working. still need to see how i can spped it up

--- public/words.js
+++ public/words.js
@@ -1,8 +1,18 @@
-function getTally(cleanData){
-    cleanData.sort((a, b) => {
+
+function getTally(copy){
+  //define the new array of tallied cities
+  let cityTally = []
+    // sort data city
+    copy.sort((a, b) => {
       return a.city > b.city ? 1 : -1;
     });
-    console.log(cleanData);
+    // loop through cities and tally
+    for ( let city of copy) {
+      let last = cityTally[cityTally.length-1];
+      copy = (last && last.city === city.city) ? last.tally++ : cityTally.push({ city: `${city.city}`, tally: 1 });
+      console.log(cityTally);   
+    }
+  
   }
 
 // get data from csv
@@ -16,9 +26,9 @@ d3
   //clone data so I am not messing with source
   
 
-	.then(function(data) {
-    const cleanData = [...data]
-		getTally(cleanData);
+	.then(function(data) {   
+    const copy = [...data]
+		getTally(copy);
 		d3.select('body').selectAll('h2').data(data).enter().append('h2').text((d) => d.city);
 	});
 
